d1f9a9379d2b64f22d81f54d16f564372b33f4ea
Vlad Hnidyi <vgnedoj03@gmail.com>
2024-08-25 14:00:33 +0300

    hwjs12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,11 +1,15 @@
-export const fetchByUserKey = userKey => {
-  const searchParams = new URLSearchParams({
-    key: '45511756-f868b950a5b29611a8f235e23',
-    q: `${userKey.trim()}`,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
+import axios from 'axios';
 
-  return fetch(`https://pixabay.com/api/?${searchParams}`);
+export const fetchByUserKey = (userKey, page) => {
+  return axios.get('https://pixabay.com/api/', {
+    params: {
+      key: '45511756-f868b950a5b29611a8f235e23',
+      q: `${userKey.trim()}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 15,
+      page,
+    },
+  });
 };
